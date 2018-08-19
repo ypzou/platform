e3a6e3e53a0c9c4e03b855a473855edac51d8caa
zouongpan <zyp187399@sina.com>
2018-08-19 23:02:26 +0800

    Thrid commit: 解决数据库高版本的问题 & HelloWorld demo

---
+++ src/main/java/com/jianbo/platform/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.jianbo.platform.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/hello")
+	public String hello(){
+		return "hello, world!";
+	}
+}
